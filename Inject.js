e1aa9ac03a0a93ddd5a38ebaee861fb9cae54671
LukasExists <91223726+lukasexists@users.noreply.github.com>
2022-10-13 14:47:28 -0400

    Update Inject.js

--- Inject.js
+++ Inject.js
@@ -1,6 +1,6 @@
 (function() {
     var x = document.createElement("script");
     x.src = "https://cdn.jsdelivr.net/gh/SnowLord7/devconsole@master/main.js";
-    x.onload = alert("Loaded Developer Console!");
+    x.onload = alert("Developera console loaded.");
     document.head.appendChild(x);
 })()
